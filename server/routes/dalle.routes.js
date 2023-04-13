57ad358bb2a684edc6d77b3ead1233bc5f4f16d8
Enrique <enrique@diontraining.com>
2023-04-13 13:49:14 -0700

    initial commit

---
+++ server/routes/dalle.routes.js
@@ -0,0 +1,40 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const router = express.Router();
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "Hello from DALL.E ROUTES" });
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const response = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    const { data } = response;
+
+    const image = data.data[0].b64_json;
+
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+});
+
+export default router;
